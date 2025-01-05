af07a5d0390d388955093f324e1822650e6a3bff
vinay.b <vinay.b@darwinbox.io>
2025-01-05 20:40:31 +0530

    Added Redirect URL in env

--- middleware.js
+++ middleware.js
@@ -4,8 +4,9 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateJWT = (req, res, next) => {
     const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    console.log(token);
     if (!token) {
-         return res.redirect('/');
+         return res.redirect(process.env.REDIRECT_URL || '/');
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
